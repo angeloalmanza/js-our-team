f4f85b033a76a94b7683a5bd2e594aadd0326948
angeloalmanza <angeloalmanza26@gmail.com>
2024-11-13 18:03:20 +0100

    feat: implementazione card e bonus responsive

--- js/script.js
+++ js/script.js
@@ -37,20 +37,59 @@ const teamMembers = [
   }
 ];
 
+// const teamList = document.getElementById("team-list");
+
+// // Lista di prova con gli oggetti dell'array
+// for(let i = 0; i < teamMembers.length; i++){
+//   const curItem = teamMembers[i];
+//   const teamListElem = document.createElement("li");
+
+//   teamListElem.innerHTML = `
+//   <img src="${curItem.img}" alt="${curItem.name}">
+//   <h3>${curItem.name}</h3>
+//   <p>Role: ${curItem.role}</p>
+//   <p>Email: <a href="${curItem.email}">${curItem.email}</a></p>
+//   `;
+
+//   teamList.append(teamListElem);
+// }
+
 // Prelevo i valori dall'html
-const teamList = document.getElementById("team-list");
+const row = document.getElementById("team-row");
 
-// Lista di prova con gli oggetti dell'array
-for(let i = 0; i < teamMembers.length; i++){
-  const curItem = teamMembers[i];
-  const teamListElem = document.createElement("li");
+/**
+ * Description
+ * @param {object} member
+ * @returns {string}
+ */
 
-  teamListElem.innerHTML = `
-  <img src="${curItem.img}" alt="${curItem.name}">
-  <h3>${curItem.name}</h3>
-  <p>Role: ${curItem.role}</p>
-  <p>Email: <a href="${curItem.email}">${curItem.email}</a></p>
-  `;
+const createCard = (members) => {
+  const { name, role, email, img } = members;
+  return `
+      <div class="col-12 col-md-6 col-lg-4">
+        <div class="team-card d-flex align-items-center mb-5">
+          <div class="card-image">
+            <img src="./${img}" alt="${name}"/>
+          </div>
+          <div class="card-text bg-dark">
+            <h3 class="text-white">${name}</h3>
+            <p class="text-white">${role}</p>
+            <a href="">${email}</a>
+          </div>
+        </div>
+      </div>
+     `;
+};
 
-  teamList.append(teamListElem);
-}
+
+const renderTeam = () => {
+  let rowItems = "";
+  for (let i = 0; i < teamMembers.length; i++) {
+    const card = createCard(teamMembers[i]);
+    rowItems += card;
+  }
+  row.innerHTML = rowItems;
+};
+
+// Chiamata funzione
+renderTeam();
